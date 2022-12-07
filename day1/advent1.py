e71583a82c386215e0eebeb10a93e51ace68eb57
Marco Ris <ris@contria.com>
2022-12-07 07:36:46 +0100

    Add day1

---
+++ day1/advent1.py
@@ -0,0 +1,36 @@
+def get_largest(arr, n):
+    harr = []
+    m = len(arr)
+
+    for i in range(0, n):
+        mx = arr[0]
+        for j in range(1, m):
+            if arr[j] > mx and arr[j] not in harr:
+                mx = arr[j]
+        harr.append(mx)
+
+    return sum(harr)
+
+
+def num_larger():
+    summation = 0
+    array = []
+    with open("input.txt") as file:
+        for line in file:
+            number = line.replace("\n", "")
+            try:
+                if number:
+                    summation += int(number)
+                else:
+                    array.append(summation)
+                    summation = 0
+            except ValueError:
+                pass
+
+    return array
+
+
+data = num_larger()
+
+print(get_largest(data, 1))  # part 1
+print(get_largest(data, 3))  # part 2
